09dfa279da5efc0584c9f707ee1f44406a3b3135
Abhishek Sachdeva <sachdeva351@gmail.com>
2015-03-24 20:31:21 +0530

    Set NewsItemListFragment to FrameLayout of MainActivity

--- app/src/main/java/com/mdg/hackernews/MainActivity.java
+++ app/src/main/java/com/mdg/hackernews/MainActivity.java
@@ -1,17 +1,39 @@
 package com.mdg.hackernews;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+
+        NavDrawerFragment navDrawerFragment = (NavDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+
+        navDrawerFragment.setup(R.id.fragment_navigation_drawer,
+                (DrawerLayout) findViewById(R.id
+                        .drawer_layout),
+                mToolbar);
+
+        NewsItemListFragment newsItemListFragment = new NewsItemListFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,newsItemListFragment)
+        .commit();
+
     }
 
 
---
+++ app/src/main/java/com/mdg/hackernews/NewsItemListFragment.java
@@ -0,0 +1,21 @@
+package com.mdg.hackernews;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abhishek on 24/3/15.
+ */
+public class NewsItemListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news_list, container, false);
+
+        return view;
+    }
+}
